7097e631017cb0c97cca6f05a2368deb00c7d191
matejikj <matejikj>
2022-06-23 06:31:40 +0200

    cosmos done, LAB5 done

--- app/app/Pages/Index.cshtml.cs
+++ app/app/Pages/Index.cshtml.cs
@@ -7,10 +7,16 @@ namespace app.Pages
 {
 	public class IndexModel : PageModel
 	{
+
 		// TODO: Hodnoty by bylo správné dát do konfigurace. Pro přehlednost a mít vše na jednom místě za účelem studia, nechávám tuto zjednodušenou variantu.
 		private const string AzureStorageConnectionString = "DefaultEndpointsProtocol=https;AccountName=storagematejik;AccountKey=NprqIlhv+oZnA2aZv8cdwIqJSD8O8xTt6isF7j//4FAK7qZqPoZbP7pZ4MIK/LumtDTOmFtLB1a/ClI4+rc36A==;EndpointSuffix=core.windows.net";
 		private const string AzureStorageContainerName = "matejikcontainer";
+		private readonly ILogger<IndexModel> _logger;
 
+		public IndexModel(ILogger<IndexModel> logger)
+		{
+			_logger = logger;
+		}
 		public List<BlobItem>? Blobs { get; set; }
 
 		public void OnGet()
@@ -20,6 +26,7 @@ namespace app.Pages
 
 		public FileStreamResult OnGetDownloadBlob(string blobName)
 		{
+			this._logger.LogInformation("getting blobs.");
 			var blobClient = CreateClient().GetBlobClient(blobName);
 			var contentType = blobClient.GetProperties().Value.ContentType;
 			var stream = blobClient.OpenRead(); // není třeba řešit Dispose - stream nám uzavře infrastruktura Razor Pages.
--- app/app/Program.cs
+++ app/app/Program.cs
@@ -2,6 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddApplicationInsightsTelemetry(builder.Configuration["APPLICATIONINSIGHTS_CONNECTION_STRING"]);
 
 var app = builder.Build();
 
@@ -12,7 +13,6 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
