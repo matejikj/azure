b5c1c093d319490093e106c91c3a8687fbc3e804
matejikj <matejikj>
2022-06-23 05:19:33 +0200

    LAB3 and Cosmos done

--- app/app/Pages/Index.cshtml.cs
+++ app/app/Pages/Index.cshtml.cs
@@ -1,20 +1,34 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace app.Pages
 {
-    public class IndexModel : PageModel
-    {
-        private readonly ILogger<IndexModel> _logger;
+	public class IndexModel : PageModel
+	{
+		// TODO: Hodnoty by bylo správné dát do konfigurace. Pro přehlednost a mít vše na jednom místě za účelem studia, nechávám tuto zjednodušenou variantu.
+		private const string AzureStorageConnectionString = "DefaultEndpointsProtocol=https;AccountName=storagematejik;AccountKey=NprqIlhv+oZnA2aZv8cdwIqJSD8O8xTt6isF7j//4FAK7qZqPoZbP7pZ4MIK/LumtDTOmFtLB1a/ClI4+rc36A==;EndpointSuffix=core.windows.net";
+		private const string AzureStorageContainerName = "matejikcontainer";
 
-        public IndexModel(ILogger<IndexModel> logger)
-        {
-            _logger = logger;
-        }
+		public List<BlobItem>? Blobs { get; set; }
 
-        public void OnGet()
-        {
+		public void OnGet()
+		{
+			Blobs = CreateClient().GetBlobs().ToList();
+		}
 
-        }
-    }
+		public FileStreamResult OnGetDownloadBlob(string blobName)
+		{
+			var blobClient = CreateClient().GetBlobClient(blobName);
+			var contentType = blobClient.GetProperties().Value.ContentType;
+			var stream = blobClient.OpenRead(); // není třeba řešit Dispose - stream nám uzavře infrastruktura Razor Pages.
+			return new FileStreamResult(stream, contentType);
+		}
+
+		private BlobContainerClient CreateClient()
+		{
+			return new Azure.Storage.Blobs.BlobContainerClient(AzureStorageConnectionString, AzureStorageContainerName);
+		}
+	}
 }
